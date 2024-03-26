a8ed322b0e3d046e37c29dc104c69079561044ed
AlexLee09 <alexandrekmlee@gmail.com>
2024-03-26 22:58:14 +0800

    Setup of Dummy Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <div>
-      <h1>Chess Coach AI</h1>
-    </div>
+    <Router>
+    <Routes>
+
+      <Route path="/" element={<LandingPage />} />
+
+      <Route path="/login" element={<LoginPage />} />
+
+    </Routes>
+  </Router>
+
   );
 }
 
