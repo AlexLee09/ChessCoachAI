ffc0c91a6ece7c6d886392ca6ee0eca9d8f0dbae
AlexLee09 <alexandrekmlee@gmail.com>
2024-04-06 14:16:18 +0800

    Add session to store login in backend

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const mongoStore = require("connect-mongo"); 
 
 const userModel = require('./models/user'); 
 
@@ -12,6 +14,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: mongoStore.create({
+            mongoUrl: process.env.MONGODB_URI,
+        }),
+        cookie: { httpOnly: false, secure: false }
+    })
+);
 
 mongoose.set('strictQuery', false);
 mongoose.connect (process.env.MONGODB_URI)
@@ -41,6 +55,8 @@ app.post("/user/new", async(req, res) =>
 
         console.log ("Created new user with username: " + username); 
 
+        req.session.userId = newUser._id; 
+
         const dataToSend = {
             status: "SUCCESS",
             message: "Account created successfully",
@@ -67,8 +83,10 @@ app.post("/user/login", async(req, res) =>
         }); 
 
         if (!user){
-            res.status(404).send({ status: "ERROR", message: "Username or password is incorrect" }); 
-            return}
+            return res.status(404).send({ status: "ERROR", message: "Username or password is incorrect" }); 
+        }
+
+        req.session.userId = user._id; 
 
         const dataToSend = {
             status: "SUCCESS", 
@@ -84,6 +102,34 @@ app.post("/user/login", async(req, res) =>
     }
 });
 
+// Check if user is already logged in, and return the user object if they are
+app.get("/user/get", async(req, res) => 
+{
+    try
+    {
+        const userId = req.session.userId; 
+
+        if (!userId)
+            return res.status(403).json({ status: "ERROR", message: "User is not logged in" })
+
+        const user = await userModel.findById(userId); 
+
+        if (!user) 
+            return res.status (403).json({ status: "ERROR", message: "User not found" })
+
+        const dataToSend = {
+            user: user, // todo: remove password from returned object
+            status: "SUCCESS",
+            message: "Found user"
+        }; 
+
+        return res.status(200).json(dataToSend); 
+    }
+    catch (error)
+    {
+        res.status(500).json({ message: "Server error" }) 
+    }
+})
 
 
 
