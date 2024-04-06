f81758b8d2568f6b61518374410ab76c60f26ffb
AlexLee09 <alexandrekmlee@gmail.com>
2024-04-06 13:57:08 +0800

    Landing page styles and images

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import classes from './Navbar.module.css';
 import logo from '../assets/logo.svg'; 
-import chessCoachAI_Logo from '../assets/ChessCoachAI_Logo.png'
+import chessCoachAI_Logo from '../assets/ChessCoachAI_Logo.png';
+import knightLogo from '../assets/Knight-Logo.png'
 
 function Navbar ()
 {
@@ -22,7 +23,7 @@ function Navbar ()
     return (
         <div className = {classes.container}>
             <div className = {classes.leftNav}>
-                <img className = {classes.logo} src={chessCoachAI_Logo} />
+                <img className = {classes.logo} src={knightLogo} />
                 <h1 className = {classes.logoText} onClick = {toLandingPage}>Chess Coach AI</h1>
             </div>
             <div className = {classes.rightNav}>
--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import { useNavigate } from 'react-router-dom';
 import classes from './LandingPage.module.css';
+import rightSideImage from '../assets/RightSideImage.png'
 
 function LandingPage () 
 {
@@ -20,15 +21,18 @@ function LandingPage ()
     return (
         <div className = {classes.container}>
             <div className = {classes.leftContainer}>
-                <h1 className = {classes.title}>Think Like A Grandmaster!</h1>
-                <h2 className = {classes.subheading}>Responses In Seconds!</h2>
+                <div className = {classes.leftText}>
+                    <h1 className = {classes.title}>Think Like A Grandmaster!</h1>
+                    <h2 className = {classes.subheading}>Responses In Seconds!</h2>
+                </div>
                 <div className = {classes.infoButtons}>
                     <button className = {classes.infoButton}>About Us</button>
                     <button className = {classes.infoButton}>Learn More</button>
                 </div>
             </div>
             <div className = {classes.rightContainer}>
-                <h1>Right Side</h1>
+                <img className = {classes.rightImage} src = {rightSideImage}></img>
+
 
             </div>
             
